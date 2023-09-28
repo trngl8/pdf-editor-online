5cfca74c98bf09d7b30786f6817f7def1de6e00e
Red Rubber Duck <mvch@triangle.software>
2023-09-28 15:58:08 +0300

    fix duplicates

--- pdf.py
+++ pdf.py
@@ -2,9 +2,7 @@ from pypdf import PdfReader, PdfWriter
 
 reader = PdfReader("var/data/example.pdf")
 writer = PdfWriter()
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-
-writer.add_page(reader.pages[0])
+page = reader.pages.pop(0)
+writer.add_page(page)
 with open("var/data/example-modified.pdf", "wb") as fp:
     writer.write(fp)
